0652d0c69a56ed95f4459ae9376b9ec3c9620a88
GhostRiderGaming <hv90507@gmail.com>
2024-10-25 14:38:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function display(){
+    document.getElementById("display").innerHTML="Introduction to Javascript";
+    document.getElementById("display").style.color="green";
+}
+function reset(){
+    document.getElementById("display").innerHTML="Introduction to Css";
+    document.getElementById("display").style.color="brown";
+}
